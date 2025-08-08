de2763cd57d2b04bd1e9bafb5bb633dd1c0e2b53
Jason Varga <jason@pixelfear.com>
2025-08-08 12:43:52 -0400

    wip

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -8,7 +8,9 @@ class ExampleTest extends TestCase
 {
     public function test_example()
     {
-        echo 'LC_ALL: '.LC_ALL;
+        echo 'LC_ALL: '.LC_ALL.PHP_EOL;
+        echo 'sqlsrv: '.(extension_loaded('sqlsrv') ? 'loaded' : 'not loaded').PHP_EOL;
+
         $this->assertEquals(0, str_word_count('мама', 0, null));
     }
 }
