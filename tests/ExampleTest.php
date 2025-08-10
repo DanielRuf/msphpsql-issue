ac33309c739bb27448ee12d49e2347a49e32abba
Daniel Ruf <daniel@daniel-ruf.de>
2025-08-11 01:44:22 +0200

    Print value of ini setting

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -8,6 +8,7 @@ class ExampleTest extends TestCase
 {
     public function test_example()
     {
+        echo ini_get('pdo_sqlsrv.set_locale_info') . PHP_EOL;
         echo 'LC_ALL: '.LC_ALL.PHP_EOL;
         echo 'sqlsrv: '.(extension_loaded('sqlsrv') ? 'loaded' : 'not loaded').PHP_EOL;
 
